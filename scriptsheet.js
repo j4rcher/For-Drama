8c8e0dc378f41f6ef1fb0b053d8609c6d2b35edc
j4rcher <huxp.ja@gmail.com>
2020-08-29 11:21:46 +0100

    added gecko infobox etc + added gecko image

--- scriptsheet.js
+++ scriptsheet.js
@@ -2,7 +2,11 @@ function openPunchdrunk() {
   document.getElementById("punchdrunk").style.animation = "fade-in 1s forwards";
 }
 
+function openGecko() {
+  document.getElementById("gecko").style.animation = "fade-in 1s forwards";
+}
 
 function closeBtn() {
   document.getElementById("punchdrunk").style.animation = "fade-out 1s forwards";
+  document.getElementById("gecko").style.animation = "fade-out 1s forwards";
 }
