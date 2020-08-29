3a2807b42fc0dfc3b154daa3f45d643bcce3f1ce
j4rcher <huxp.ja@gmail.com>
2020-08-29 11:30:41 +0100

    added paperbirds infobox etc + added paperbirds image

--- scriptsheet.js
+++ scriptsheet.js
@@ -6,7 +6,12 @@ function openGecko() {
   document.getElementById("gecko").style.animation = "fade-in 1s forwards";
 }
 
+function openPaperBirds() {
+  document.getElementById("paper-birds").style.animation = "fade-in 1s forwards";
+}
+
 function closeBtn() {
   document.getElementById("punchdrunk").style.animation = "fade-out 1s forwards";
   document.getElementById("gecko").style.animation = "fade-out 1s forwards";
+  document.getElementById("paper-birds").style.animation = "fade-out 1s forwards";
 }
